5802f1422c9c98178462d55eb6d1def521646883
Ellen Lo <lowing@bu.edu>
2023-11-18 21:53:32 -0800

    test in xr: fix pikmin never stop issue

--- Assets/Pikmin/Scripts/Misc/FlockBehavior.cs
+++ Assets/Pikmin/Scripts/Misc/FlockBehavior.cs
@@ -36,14 +36,15 @@ public class FlockBehavior : MonoBehaviour
     [Header("Behavior variables")]
     [SerializeField] private float _speed;
     [SerializeField] private float _distanceFromLeader;
+    [SerializeField] private float _distanceThreshold;
     [SerializeField] private Vector2 _gridSize;
     [SerializeField] private float _gridSpace;
     [SerializeField] private int _numBoids;
     [SerializeField] private float _leaderChangeRate;
-    private float _noise;
+    [SerializeField] private float _noise;
+    [SerializeField] private BoidFormation _boidFormation;
 
     private List<Boid> boids;
-    private BoidFormation boidFormation;
 
     private Vector3 lastLeaderPosition;
     private GameObject leaderGhost;
@@ -52,15 +53,23 @@ public class FlockBehavior : MonoBehaviour
     {
         InitializeBoids();
         leaderGhost = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        leaderGhost.transform.position = leaderTransform.position;
+        leaderGhost.transform.position = GetNewPosition(Vector3.zero);
         leaderGhost.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        Rigidbody ghostRb = leaderGhost.AddComponent<Rigidbody>();
+        ghostRb.interpolation = RigidbodyInterpolation.Interpolate;
+        ghostRb.useGravity = false;
     }
 
     void Update()
     {
         float step = _speed * Time.deltaTime;
 
-        bool leaderAwayFromPack = Vector3.Distance(leaderGhost.transform.position, leaderTransform.position) > _distanceFromLeader; // maybe replace distance from leader to a smaller threshold
+        Vector3 groundedTransformPosition = new Vector3(
+            leaderTransform.position.x,
+            0,
+            leaderTransform.position.z
+        );
+        bool leaderAwayFromPack = Vector3.Distance(leaderGhost.transform.position, groundedTransformPosition) > _distanceThreshold; 
 
         if(leaderAwayFromPack) 
         {
@@ -73,28 +82,24 @@ public class FlockBehavior : MonoBehaviour
             Boid boid = boids[i];
             Transform boidTransform = boid.gameObject.transform;
 
+            Vector3 newPosition = GetNewPosition(boid.positionOffset);
+
             if(!leaderAwayFromPack)
             {
                 // If pikmin has arrived at new position
-                boidTransform.rotation = Quaternion.LookRotation(leaderTransform.forward, Vector3.up);
+                boidTransform.rotation = Quaternion.LookRotation(groundedTransformPosition - boidTransform.position, Vector3.up);
                 boid.anim.SetInteger("state", 0);
             }
             else
             {
                 // If pikmin is still walking
-                Vector3 newPosition = GetNewPosition(boid.positionOffset);
                 boidTransform.position = Vector3.MoveTowards(boidTransform.position, newPosition, step);
                 boidTransform.rotation = Quaternion.LookRotation(newPosition - boidTransform.position, Vector3.up);
                 boid.anim.SetInteger("state", 1);
             }
         }
     }
-
-    void OnValidate()
-    {
-        UpdateFormation();
-    }
-
+    
     private void InitializeBoids()
     {
         boids = new List<Boid>();
@@ -119,7 +124,7 @@ public class FlockBehavior : MonoBehaviour
     {
         Vector3 newOffset = leaderTransform.forward * positionOffset.z + leaderTransform.right * positionOffset.x;
         Vector3 newPosition = leaderTransform.position + newOffset;
-        if(boidFormation == BoidFormation.Radial)
+        if(_boidFormation == BoidFormation.Radial)
         {
             newPosition += newOffset * (_distanceFromLeader - 1f);
         }
@@ -127,6 +132,7 @@ public class FlockBehavior : MonoBehaviour
         {
             newPosition += newOffset - leaderTransform.forward * _distanceFromLeader;
         }
+
         newPosition.y = 0;
         
         return newPosition;
@@ -134,17 +140,17 @@ public class FlockBehavior : MonoBehaviour
 
     private Vector3 GetPositionOffset(int index)
     {
-        if(boidFormation == BoidFormation.Line)
+        if(_boidFormation == BoidFormation.Line)
         {
             return new Vector3(0, 0, -index * _gridSpace);
         }
-        else if (boidFormation == BoidFormation.Grid)
+        else if (_boidFormation == BoidFormation.Grid)
         {
             float x = Mathf.Floor(index % _gridSize.x);
             float z = Mathf.Floor(index / _gridSize.x);
             return new Vector3(-x * _gridSpace, 0f, -z * _gridSpace);
         }
-        else if (boidFormation == BoidFormation.Radial)
+        else if (_boidFormation == BoidFormation.Radial)
         {
             float angle = (float)index / (float)_numBoids * Mathf.PI * 2f;
             float x = Mathf.Cos(angle);
